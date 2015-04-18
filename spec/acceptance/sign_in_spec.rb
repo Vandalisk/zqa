a13af851040a3144e420e010bbc54e4b0de8a5c4
ILya <zakharuni13@gmail.com>
2015-04-18 15:02:38 +0400

    rewrite sign_in_specs

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 feature 'Siging in', %q{
   In order to be able ask questions
@@ -6,6 +6,27 @@ feature 'Siging in', %q{
   I want be able to sign in
  } do
 
+	scenario 'Registered user try to sign in' do
+		User.create!(email: 'user@test.com', password: '12345678')
+		visit new_user_session_path
+		fill_in 'Email', with: 'user@test.com'
+		fill_in 'Password', with: '12345678'
+		click_on 'Sign in'
+
+		expect(page).to have_content 'Signed in successfully.'
+		expect(current_path).to eq root_path
+    end
+
+    scenario 'Non-registered user try to sign in' do
+		visit new_user_session_path
+		fill_in 'Email', with: 'wrong@user.com'
+		fill_in 'Password', with: '12345'
+		click_on 'Sign in'
+
+		expect(page).to have_content 'Invalid email or password.'
+		expect(current_path).to eq new_user_session_path
+	qend
+=begin
   given(:user) { create(:user) }
 
   scenario "Existing user try to sign in" do
@@ -22,4 +43,5 @@ feature 'Siging in', %q{
 
     expect(page).to have_content 'Invalid email or password.'
   end
+=end
 end
