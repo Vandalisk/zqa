f25d4edfa3de3b7d71ce39b048ff1d574f0ab327
ILya <zakharuni13@gmail.com>
2015-05-05 14:02:30 +0400

    without check_authorization better

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,4 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
   	redirect_to root_url, alert: exception.message
   end
-
-  check_authorization
 end
