7f58a00489104535c045e26bddcabb4abe8e4354
ILya <zakharuni13@gmail.com>
2015-05-05 13:57:29 +0400

    added cancan. Authorization is warking fine. error in browser because in application_controller 'check_authorization' and cancan swears

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
 	validates :title, :body, presence: true
 	
+	belongs_to :user
 	has_many :answers
 	has_many :attachments, as: :attachmentable
 	has_many :comments, as: :commentable
