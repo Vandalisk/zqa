55d646ec96c99bf9e168c7645ef15771db552c3d
ILya <zakharuni13@gmail.com>
2015-05-13 12:56:19 +0400

    Eng and Rus localization

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,6 @@ module Zqa
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :delayed_job
+    config.i18n.available_locales = [:en, :ru]
   end
 end
